f0ae9cd684a2e2c4d5450202c7fc1b3aef72ffac
lsfernandes92 <lsfernandes92@gmail.com>
2023-12-28 15:19:19 -0300

    Add how to use context

---
+++ spec/using_context_keyword_spec.rb
@@ -0,0 +1,11 @@
+describe 'Using context keyword' do
+  # The context block can be used to separate situations
+  # while writing your tests
+  context 'when API has the correct headers' do
+    xit 'returns correct response'
+  end
+
+  context 'when API has no headers' do
+    xit 'returns status code 415'
+  end
+end
